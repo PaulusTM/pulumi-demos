5c4ad477d1326adefce05cd73937fe0ae41e3e72
Daniel Paulus <d.paulus@fullstaq.com>
2020-03-30 22:07:08 +0200

    Add demo1
    Signed-off-by: Daniel Paulus <d.paulus@fullstaq.com>

---
+++ demo1/index.ts
@@ -0,0 +1,15 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure";
+
+// Create an Azure Resource Group
+const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
+
+// Create an Azure resource (Storage Account)
+const account = new azure.storage.Account("storage", {
+    resourceGroupName: resourceGroup.name,
+    accountTier: "Standard",
+    accountReplicationType: "LRS",
+});
+
+// Export the connection string for the storage account
+export const connectionString = account.primaryConnectionString;
